84cc80153305eb813f0820f962f7fcccb1ea9652
limsammy <limsammy1@gmail.com>
2018-06-16 02:42:04 -0600

    Spec failing

--- spec/playlist_grabber_test.rb
+++ spec/playlist_grabber_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+require "minitest/autorun"
 
 class PlaylistGrabberTest < Minitest::Test
-  def grabs_playlist_and_returns_list
+  def test_grabs_playlist_and_returns_list
     videos = PlaylistGrabber.grab_playlist('')
     assert videos == 2
   end
--- spec/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "myproject"
-
-require "minitest/autorun"
