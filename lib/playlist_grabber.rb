24b9b607c0c81b1ad4ba69bb3098bb90d4f4a36e
limsammy <limsammy1@gmail.com>
2018-06-16 03:14:36 -0600

    Refactor

--- lib/playlist_grabber.rb
+++ lib/playlist_grabber.rb
@@ -4,14 +4,12 @@ require 'yt'
 require 'pry'
 
 class PlaylistGrabber
-  # def initialize
-  #   Yt.configuration.api_key = ENV['YT_KEY']
-  # end
+  def initialize
+    Yt.configuration.api_key = ENV['YT_KEY']
+  end
 
   def grab_playlist(id)
-    Yt.configuration.api_key = ENV['YT_KEY']
     playlist = Yt::Playlist.new(id: id)
-    # binding.pry
     playlist.playlist_items.map do |video|
       video.video_id
     end
