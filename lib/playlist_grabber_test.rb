5edb0008e89777d781dfdc2bb1982fa903ef272a
limsammy <limsammy1@gmail.com>
2018-06-16 02:36:48 -0600

    Added minitest requirement

---
+++ lib/playlist_grabber_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+class PlaylistGrabberTest < Minitest::Test
+  def grabs_playlist_and_returns_list
+    videos = PlaylistGrabber.grab_playlist('')
+    assert videos == 2
+  end
+end
