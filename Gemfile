6c818aeae67ad6e1170cbe686446a5a83e04561a
limsammy <limsammy1@gmail.com>
2018-06-16 02:50:14 -0600

    Instaleld dotenv

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'minitest'
 gem 'yt', '~> 0.28.0'
+gem 'dotenv'
--- lib/playlist_grabber.rb
+++ lib/playlist_grabber.rb
@@ -1,5 +1,14 @@
 require 'yt'
+require 'dotenv'
 
 class PlaylistGrabber
+  def setup
+    Yt.configuration.api_key = ENV['api_key']
+  end
 
+  def self.grab_playlist(id)
+    setup
+    playlist = Yt::PlaylistItem.new(id: 'PLjW_GNR5Ir0GWEP_ove')
+    playlist
+  end
 end
